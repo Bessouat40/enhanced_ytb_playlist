dd9656432f2dfad589bab48cdf22d91cd5c8ca8e
duranbe <benoit.durand.mail@gmail.com>
2021-01-12 13:50:01 +0100

    change id, fix code, link to ytber collection

--- scrap_playlist/recup_bdd.py
+++ scrap_playlist/recup_bdd.py
@@ -9,26 +9,32 @@ from selenium.webdriver.chrome.options import Options
 from pymongo import MongoClient #création de notre collection qui va contenir les infos
 
 #pour l'exemple on initialise une liste de noms de youtubeurs qui va simuler ce qu'on utilisera plus tard
-liste_ytbeurs = {'id' : ['UCWeg2Pkate69NFdBeuRFTAw','UCI0vQvr9aFn27yR6Ej6n5UA','UCIlUBOXnXjxdjmL_atU53kA','UCMV8ybQXuvInI5lYw2Wzfjw'] ,
-                 'chaine' : ['squeezie','Real Python', 'Cours Python 3','Python 3 Dersleri']}
+#liste_ytbeurs = {'id' : ['UCWeg2Pkate69NFdBeuRFTAw','UCI0vQvr9aFn27yR6Ej6n5UA','UCIlUBOXnXjxdjmL_atU53kA','UCMV8ybQXuvInI5lYw2Wzfjw'] ,
+#                 'chaine' : ['squeezie','Real Python', 'Cours Python 3','Python 3 Dersleri']}
 
 #pour que la fonction s'exécute sans erreur, il faut que le fichier chromedriver soit dans le même répertoire que ce fichier python
 def recup_bdd_headless(id_chaine) :
-    """ Fonction qui prend en entrée le nom de la chaîne youtube
+    """ 
+    Fonction qui prend en entrée le nom de la chaîne youtube
     dont ou souhaite récupérer les informations
-    arg chaine : string correspondant au nom de la chaîne youtube
-    return : dictionnaire comportant plusieurs informations sur la chaîne youtube"""
-    IS_LINUX = False
+    
+    Arg: 
 
+        chaine: string correspondant au nom de la chaîne youtube
+    
+    Returns: 
+
+        dictionnaire comportant plusieurs informations sur la chaîne youtube"""
+    
     CHROME_PATH = "./chromedriver"
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome = webdriver.Chrome(executable_path=CHROME_PATH,
-                              chrome_options=chrome_options
+                              options=chrome_options
                              )
 
-    #récuparation des données
+    #récupération des données
     bdd = []
     lien = 'https://www.youtube.com/channel/{}/playlists?view=1&sort=dd&shelf_id=0'.format(id_chaine)
     chrome.get(lien)
@@ -49,6 +55,8 @@ def recup_bdd_headless(id_chaine) :
     l_title = list(filter(None,l_title))
 
     for idx,i in enumerate(l_url) :
+        
+        
         try :
             chrome.get(i)
             tag = chrome.find_element_by_id("stats")
@@ -66,25 +74,37 @@ def recup_bdd_headless(id_chaine) :
 
             vues = chrome.find_element_by_xpath("//yt-formatted-string[2]").text
             vues = vues.replace('\u202f','')
-            bdd.append({'id_youtubeur' : id_chaine,
+            id_playlist = re.findall(r'https:\/\/www\.youtube\.com\/playlist\?list=([a-zA-Z0-9-_]*)',i)[0]
+
+            bdd.append({ '_id' : id_playlist,
+                    'id_youtubeur' : id_chaine,
                     'playlist' : l_title[idx],
                     'nbr_videos' : nbr_videos,
                     'derniere_MAJ' : last_modif,
                     'vues' : vues
                    })
-        except : None
+        except:
+            pass
+
     chrome.quit()
     return bdd
 
 #ici on commence à créer la bdd Mongo
 client = MongoClient()
-db_test = client['youtube']
 
-col_youtubeur = db_test['id_youtubeur']
-for i in range(len(liste_ytbeurs)):
-    col_youtubeur.insert_one({'id' : liste_ytbeurs['id'][i], 'chaine' : liste_ytbeurs['chaine'][i]})
+db = client['projet']
+collection = db['channels']
+ytb_list = list(collection.find({}))
+
+liste_ytbeurs = [v['_id'] for v in ytb_list]
+db_test = client['youtube']
 col_playlist = db_test['playlist']
 data=[]
-for i in range(len(liste_ytbeurs['id'])):
-    data = recup_bdd_headless(liste_ytbeurs['id'][i])
-    col_playlist.insert_many(data)
+for i in range(len(liste_ytbeurs)):
+
+    print(i,'/',len(liste_ytbeurs))
+    data = recup_bdd_headless(liste_ytbeurs[i])
+    try:
+        col_playlist.insert_many(data)
+    except:
+        pass
