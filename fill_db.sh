213d1ecd7a9f0d2f73efa90771e725826557359a
duranbe <benoit.durand.mail@gmail.com>
2021-02-07 19:15:45 +0100

    comment script

--- fill_db.sh
+++ fill_db.sh
@@ -1,3 +1,6 @@
+#Obtaining the channels 
 python ./ytb-api/get_channels.py
+#Obtaining the playlist linked to these channels
 python ./scrap_playlist/recup_bdd.py
+#Obtaining the videos linked to these playlist
 python ./scrap_playlist/recup_data_video.py
